8d7e9fb695a29dd9d74a25cb40aabe5c33676f85
Cristian Varela Casas <cristianchantada@gmail.com>
2024-01-24 13:42:07 +0100

    Finalizado IBAN

--- src/ventana2/Ventana2.java
+++ src/ventana2/Ventana2.java
@@ -16,9 +16,10 @@ public class Ventana2 extends JFrame {
     private JTextField textNif;
     private JTextField textTarjetaCredito;
 
-    private final String PATH = "C:\\Users\\Usuario\\Desktop\\VentaCliente3\\src\\ventana2";
+    private final String PATH = "C:\\Users\\Profesor\\Desktop\\ventanaCliente2-main\\src\\ventana2";
     private final String FILE_NAME = "clients.txt";
     private final String PATH_TO_FILE = PATH + "\\" + FILE_NAME;
+    private JTextField textBankAccount;
 
     /**
      * Launch the application.
@@ -49,7 +50,7 @@ public class Ventana2 extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JLabel lblNewLabel = new JLabel("Cliente");
+        JLabel lblNewLabel = new JLabel("CLIENTE");
         lblNewLabel.setBounds(38, 35, 51, 26);
         contentPane.add(lblNewLabel);
 
@@ -58,22 +59,22 @@ public class Ventana2 extends JFrame {
         contentPane.add(lblNif);
 
         JLabel lblTarjetaDeCredito = new JLabel("TARJETA DE CRÉDITO");
-        lblTarjetaDeCredito.setBounds(38, 109, 120, 26);
+        lblTarjetaDeCredito.setBounds(38, 109, 141, 26);
         contentPane.add(lblTarjetaDeCredito);
 
         textNombre = new JTextField();
-        textNombre.setBounds(180, 38, 178, 20);
+        textNombre.setBounds(212, 40, 178, 20);
         contentPane.add(textNombre);
         textNombre.setColumns(10);
 
         textNif = new JTextField();
         textNif.setColumns(10);
-        textNif.setBounds(180, 75, 178, 20);
+        textNif.setBounds(212, 77, 178, 20);
         contentPane.add(textNif);
 
         textTarjetaCredito = new JTextField();
         textTarjetaCredito.setColumns(10);
-        textTarjetaCredito.setBounds(180, 112, 178, 20);
+        textTarjetaCredito.setBounds(212, 114, 178, 20);
         contentPane.add(textTarjetaCredito);
 
         JButton darAltaBoton = new JButton("Dar de alta");
@@ -81,25 +82,35 @@ public class Ventana2 extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
             	
-            	// Quito espacios y guiones a las cadenas original de NIF y tarjeta de Crédito
+            	// Quito espacios y guiones a las cadenas original de NIF, tarjeta de Crédito y cuenta bancaria.
             	textNif.setText(textNif.getText().replaceAll("\\s", "").replaceAll("-", ""));
             	textTarjetaCredito.setText(textTarjetaCredito.getText().replaceAll("\\s", "").replaceAll("-", ""));
-
-                boolean dataValidateOk = validateClientData(textNombre.getText(), textNif.getText(), textTarjetaCredito.getText());
+            	textBankAccount.setText(textBankAccount.getText().replaceAll("\\s", "").replaceAll("-", ""));
+            	
+                boolean dataValidateOk = validateClientData(textNombre.getText(), textNif.getText(), textTarjetaCredito.getText(), textBankAccount.getText());
                 boolean nifValidateOk = false;
+                boolean bankAccountOk = false;
+                
                 if(dataValidateOk) {
                 	System.out.println("Procediendo a la validación del DNI según el algoritmo.....");
                 	nifValidateOk = validateNifAlgorithm(textNif.getText());
                 }
+                
+                if(dataValidateOk) {
+                	System.out.println("Procediendo a la validación del IBAN según el algoritmo.....");
+                	bankAccountOk = validateBankAccountAlgorithm(textBankAccount.getText());
+                }
                 		
 
-                if (dataValidateOk && nifValidateOk) {
+                if (dataValidateOk && nifValidateOk && bankAccountOk) {
                 	System.out.println("Los datos del cliente y la comprobación del DNI según su algoritmo han sido pasados satisfactoriamente");
                     try {
                         FileWriter fw = new FileWriter(PATH_TO_FILE, true);
                         fw.write("Nombre: " + textNombre.getText() + " | "
-                                + "NIF: " + textNif.getText() + " | " + "Tarjeta Crédito: "
-                                + textTarjetaCredito.getText());
+                                + "NIF: " + textNif.getText() + " | Tarjeta Crédito: "
+                                + textTarjetaCredito.getText() + " | IBAN bancario: " 
+                                + textBankAccount.getText()+ "\n"
+                                );
 
                         System.out.println("El cliente " + textNombre.getText() + " ha sido resgistrado en el sistema");
                         fw.close();
@@ -109,18 +120,21 @@ public class Ventana2 extends JFrame {
                     }
 
                     verMensaje("Nombre: " + textNombre.getText() + "\n" + "NIF: " + textNif.getText() + "\n"
-                            + "Tarjeta Crédito: " + textTarjetaCredito.getText());
+                            + "Tarjeta Crédito: " + textTarjetaCredito.getText() + "\n" 
+                    		+ "IBAN bancario: " + textBankAccount.getText());
 
                     verMensaje("Registro guardado");
                     textNombre.setText("");
                     textNif.setText("");
                     textTarjetaCredito.setText("");
+                    textBankAccount.setText("");
                 } else {
                 	verMensaje("Introduzca sus datos nuevamente");
                 	System.out.println("Se inicializa la ventana con los valores a cadena vacía");
                     textNombre.setText("");
                     textNif.setText("");
                     textTarjetaCredito.setText("");
+                    textBankAccount.setText("");
                 }
             }
         });
@@ -128,34 +142,55 @@ public class Ventana2 extends JFrame {
             public void actionPerformed(ActionEvent e) {
             }
         });
-        darAltaBoton.setBounds(180, 158, 178, 23);
+        darAltaBoton.setBounds(212, 192, 178, 23);
         contentPane.add(darAltaBoton);
+        
+        textBankAccount = new JTextField();
+        textBankAccount.setColumns(10);
+        textBankAccount.setBounds(212, 150, 178, 20);
+        contentPane.add(textBankAccount);
+        
+        JLabel lblIbanBancario = new JLabel("IBAN BANCARIO");
+        lblIbanBancario.setBounds(38, 146, 141, 26);
+        contentPane.add(lblIbanBancario);
+        
+        JLabel infoMessage = new JLabel("<--- Nuevo para actividad de IBAN, resto del anterior, código aprovechado");
+        infoMessage.setBounds(400, 147, 392, 26);
+        contentPane.add(infoMessage);
+        
+        JLabel lblNewLabel_1 = new JLabel("(2 letras del país y 24 dígitos)");
+        lblNewLabel_1.setBounds(10, 170, 169, 14);
+        contentPane.add(lblNewLabel_1);
     }
 
     private void verMensaje(String mensaje) {
         JOptionPane.showMessageDialog(null, mensaje);
     }
 
-    private boolean validateClientData(String name, String nif, String creditCardNumber) {
+    private boolean validateClientData(String name, String nif, String creditCardNumber, String textBankAccount) {
 
 
-        System.out.println("El DNI formateado es " + nif + ", y el número de tarjeta de crédito formateado es " + creditCardNumber);
+        System.out.println("El DNI formateado es " + nif + "\nEl número de tarjeta de crédito formateado es " 
+        + creditCardNumber + "\nEl número de IBAN introducido y ya formateado ha sido: " + textBankAccount);
 
         // Creo las Regex para los distintos datos
         String nameRegex = "^[\\p{L}]+(?:[\\p{Zs}][\\p{L}]+)*$";
-        String nifRegex = "^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$|^[XYZxyz][0-9]{7}$";
+        String nifRegex = "^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$|^[xyzXYZ]\\d{7}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$";
         String creditCardNumberRegex = "^(\\d\\s*){16}$";
-
+        String bankAccountRegex = "^[eE][sS]\\d{24}$";
+;
 
         // Creo un objeto Pattern compilando la expresión regular
         Pattern namePattern = Pattern.compile(nameRegex);
         Pattern nifPattern = Pattern.compile(nifRegex);
         Pattern creditCardNumberPattern = Pattern.compile(creditCardNumberRegex);
+        Pattern bankAccountPattern = Pattern.compile(bankAccountRegex);
 
         // Crear un objeto Matcher y realizar la validación
         Matcher nameMatcher = namePattern.matcher(name);
         Matcher nifMatcher = nifPattern.matcher(nif);
         Matcher creditCardNumberMatcher = creditCardNumberPattern.matcher(creditCardNumber);
+        Matcher bankAccountMatcher = bankAccountPattern.matcher(textBankAccount);
 
         // Verifico si la cadena cumple con la expresión regular
         boolean checker = true;
@@ -176,6 +211,13 @@ public class Ventana2 extends JFrame {
 
         if (!creditCardNumberMatcher.matches()) {
             message = "El número de su tarjeta de crédito no es correcto, por favor, vuelva a introducirlo";
+            JOptionPane.showMessageDialog(null, message);
+            System.out.println(message);
+            checker = false;
+        }
+        
+        if (!bankAccountMatcher.matches()) {
+            message = "El número Iban de su cuenta bancaria  introducido " + textBankAccount + " no es correcto, recuerde insertar las 2 letras del país y los 24 dígitos";
             JOptionPane.showMessageDialog(null, message);
             System.out.println(message);
             checker = false;
@@ -225,8 +267,23 @@ public class Ventana2 extends JFrame {
         	System.out.println("El campo DNI ha llegado vacío al checkeo con algoritmo DNI");
         	return false;
         }
-
     }
+    
+    public boolean validateBankAccountAlgorithm(String bankAccount) {
+    	
+    	String IbanCountryLetters = bankAccount.substring(0, 2);
+    	String IbanCountryLettersUppercase = IbanCountryLetters.toUpperCase();
+    	System.out.println("El código del país introducido es " + IbanCountryLetters);
+    	
+    	if(!IbanCountryLettersUppercase.equals("ES")) {
+    		System.out.println("Las letras del código de país no coinciden con ES de España");
+    		verMensaje("Las letras de control de país en el IBAN indicado no pertenecen a España, por favor, vuelva a intentarlo");
+    		return false;
+    	} else {
+    	return true;
+    	}
 
+    
+    }
     
 }
